05611b228bab587022da1671d50cccd510c3ac58
lotyp <lotyp7@gmail.com>
2024-05-17 00:47:45 +0300

    feat: update default set rule sets

--- src/RuleSets/DefaultSet.php
+++ src/RuleSets/DefaultSet.php
@@ -33,37 +33,36 @@ final class DefaultSet implements RuleSet
     public function rules(): array
     {
         return array_merge([
-            // Base our RuleSet on Symfony
-            // PhpCsFixer\RuleSet\Sets\SymfonySet
+            /*
+             * Base our RuleSet on @Symfony RuleSet
+             * https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/ruleSets/Symfony.rst
+             * https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/src/RuleSet/Sets/SymfonySet.php
+             */
             '@Symfony' => true,
 
-            // @Symfony overrides
-            'single_line_throw' => false,
-            'ordered_imports' => [
-                'imports_order' => [
-                    'class',
-                    'function',
-                    'const',
-                ],
-            ],
+            /*
+             * @Symfony overrides
+             */
             'concat_space' => [
                 'spacing' => 'one',
             ],
             'method_argument_space' => [
-                'after_heredoc' => true,
+                'on_multiline' => 'ensure_fully_multiline',
+                'attribute_placement' => 'same_line',
+                'after_heredoc' => true, // @PHP73Migration
             ],
             'php_unit_method_casing' => [
                 'case' => 'snake_case',
             ],
             'yoda_style' => [
-                'always_move_variable' => true,
-                'equal' => true,
-                'identical' => true,
-                'less_and_greater' => true,
+                'equal' => false,
+                'identical' => false,
+                'less_and_greater' => false,
             ],
             'phpdoc_align' => [
                 'align' => 'left',
             ],
+
             'ordered_class_elements' => [
                 'sort_algorithm' => 'none',
                 'order' => [
@@ -91,57 +90,67 @@ final class DefaultSet implements RuleSet
                     'destruct',
                 ],
             ],
-
-            // @Symfony:risky overrides
-            'native_function_invocation' => [
-                'include' => [
-                    // All php native functions
-                    NativeFunctionInvocationFixer::SET_INTERNAL,
-                ],
-                'scope' => 'namespaced',
-                'strict' => true,
-            ],
-            'php_unit_test_annotation' => [
-                'style' => 'annotation',
-            ],
-
-            // @PHP80Migration
-            'list_syntax' => [
-                'syntax' => 'short',
-            ],
-            'static_lambda' => true,
-
-            // @PHP70Migration @PHP71Migration @PHP73Migration @PHP74Migration @PHP80Migration
-            'ternary_to_null_coalescing' => true,
-            'random_api_migration' => true,
-            'declare_strict_types' => true,
-
-            // @PHP71Migration:risky @PHP74Migration:risky @PHP80Migration:risky
-            'void_return' => true,
-
-            // @PhpCsFixer
-            // PhpCsFixer\RuleSet\Sets\PhpCsFixerSet
-            'array_indentation' => true,
-            'align_multiline_comment' => true,
-            'explicit_indirect_variable' => true,
-            'method_chaining_indentation' => true,
-            'phpdoc_order' => true,
-            'phpdoc_var_annotation_correct_order' => true,
-            'phpdoc_add_missing_param_annotation' => true,
-            'multiline_comment_opening_closing' => true,
-
-            // Rules without presets
-            'phpdoc_tag_casing' => true,
-            'not_operator_with_successor_space' => true,
             'global_namespace_import' => [
                 'import_classes' => true,
                 'import_functions' => true,
                 'import_constants' => true,
             ],
-            'self_static_accessor' => true,
 
+            /*
+             * @Symfony:risky overrides
+             */
+            'native_function_invocation' => [
+                'include' => [
+                    NativeFunctionInvocationFixer::SET_INTERNAL,
+                    NativeFunctionInvocationFixer::SET_COMPILER_OPTIMIZED,
+                ],
+                'scope' => 'namespaced',
+                'strict' => true,
+            ],
+
+            /*
+             * @PHP**MigrationSet
+             */
+            'list_syntax' => [
+                'syntax' => 'short',
+            ],
+            'ternary_to_null_coalescing' => true,
+
+            /*
+             * @PHP**MigrationSet:risky
+             */
+            'random_api_migration' => true,
+            'declare_strict_types' => true,
+            'void_return' => true,
+
+            /*
+             * @PhpCsFixer
+             */
+            'single_line_throw' => false,
+            'explicit_indirect_variable' => true,
+            'method_chaining_indentation' => true,
+            'phpdoc_var_annotation_correct_order' => true,
+            'phpdoc_add_missing_param_annotation' => [
+                'only_untyped' => true,
+            ],
+            'multiline_comment_opening_closing' => true,
+            'self_static_accessor' => true,
             'no_useless_else' => true,
             'no_useless_return' => true,
+
+            /*
+             * @PhpCsFixer:risky
+             */
+            'static_lambda' => true,
+            'php_unit_test_annotation' => [
+                'style' => 'annotation',
+            ],
+
+            /*
+             * Rules without presets
+             */
+            'phpdoc_tag_casing' => true,
+            'not_operator_with_successor_space' => true,
         ], $this->rules);
     }
 }
